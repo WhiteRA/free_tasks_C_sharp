c4350597dd99f1db14173827188a1b07f721b2d2
Parhomenko Aleksandr <WhiteRA_@mail.ru>
2022-12-13 21:41:44 +1000

    добавлены изменения в файл заданий

--- tasks.cs
+++ tasks.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using static library;
+/*
+int q = library.RandomNumbers_1();
+q = q*q*q;
+
+int w = library.RandomNumbers_1();
+int e = library.RandomNumbers_1();
+int r = 0;
+if(e == w*w){
+    Console.WriteLine(e +" Квадрат числа" + w);
+    Console.WriteLine(e);
+    Console.WriteLine(w);
+}else{
+    Console.WriteLine("не подходит");
+}
+
+int t = library.RandomNumbers_1();
+int y = library.RandomNumbers_1();
+if(t > y){
+    Console.WriteLine(t + " Больше чем " + y);
+}else if(t == y)  {
+    Console.WriteLine(y + " Равен " + t);
+}else{
+    Console.WriteLine(y + " Больше чем " + t);
+}
+
+int a = library.RandomNumbers_1();
+int s = library.RandomNumbers_1();
+int d = library.RandomNumbers_1();
+int asd = a;
+if(a > asd){
+    asd = a;
+    Console.WriteLine("Ответ");
+    Console.WriteLine(asd);
+}
+if (s > asd){
+    asd = s;
+    Console.WriteLine("Ответ");
+    Console.WriteLine(asd);
+}
+if (d > asd){
+    asd = d;
+    Console.WriteLine("Ответ");
+    Console.WriteLine(asd);
+}
+
+*/
