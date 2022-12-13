3a88393d0540fb92b92274fd3a5b232f77fc4ccd
Parhomenko Aleksandr <WhiteRA_@mail.ru>
2022-12-13 21:41:19 +1000

    добавлены изменения в библиотеку

--- library.cs
+++ library.cs
@@ -1,1 +1,13 @@
 ﻿
+public static class library
+{
+    /// <summary>
+    /// Метод генерирует случайное число от 1 до 10 
+    /// </summary>
+    /// <returns></returns>
+    public static int RandomNumbers_1()
+    {
+        int RandomNumbers = new Random().Next(1, 10);
+        return RandomNumbers;
+    }
+}
